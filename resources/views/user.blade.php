0c83d7cab85afe8a7f41fc7fbe38923518973daa
Fabiano Tomasini <ftomasini@bitbucket.org>
2020-09-18 16:25:00 -0300

    Testes com rotas

---
+++ resources/views/user.blade.php
@@ -0,0 +1,9 @@
+<html>
+<h1>user</h1>
+
+
+<div>
+    <a href="{{route('app')}}">App</a><br>
+    <a href="{{route('app-user')}}">User</a><br>
+    <a href="{{route('app-profile')}}">Profile</a><br>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,67 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Rota normal 
+Route::get('/teste', function() { 
+    echo "Ola!";
+});
+
+// Rota com variável parametro
+Route::get('/ola/{nome}/{sobrenome}', function($nome, $sobrenome){
+    echo "Olá! seja bem vindo. " . $nome . " " . $sobrenome;
+});
+
+
+// Rota com variável parametro condicional, nesse caso com ?
+Route::get('/seunome/{nome}/{sobrenome?}', function($nome, $sobrenome=null ){
+    if(!isset($sobrenome)){
+        return "digite um sobrenome também";
+    }
+    else{
+        echo "Olá! seja bem vindo. " . $nome . " " . $sobrenome;
+    }
+});
+
+//Rota com regras
+Route::get('/rotacomregra/{nome}/{n?}', function($nome, $n=null ){
+    
+    for($i=0;$i<=$n;$i++){
+        echo "Olá! seja bem vindo. " . $nome . " " . $n. '<br>';}
+    }
+)->where('nome','[A-Za-z]+')->where('n','[0-9]+');
+
+//Rotas com agrupamento e nomes
+Route::prefix('/app')->group(function() {
+
+    Route::get('/',function() {
+        return view('app');
+    })->name('app');
+
+    Route::get('/user',function() {
+        return view('user');
+    })->name('app-user');
+
+    Route::get('/profile',function() {
+        return view('profile');
+    })->name('app-profile');
+
+});
+
+Route::get('/produtos', function (){
+    echo "<h1>Produtos</h1>";
+    echo "<ol>";
+    echo "<li>Notebook</li>";
+    echo "<li>Impressora</li>";
+    echo "<li>Mouse</li>";
+    echo "</ol>";
+});
+
+//Redirecionamento de rotas.
+Route::redirect('todosprodutos', 'produtos', 301);
+
+Route::get('todososprodutos2', function(){
+    return redirect()->route('meusprodutos');
+});
+
