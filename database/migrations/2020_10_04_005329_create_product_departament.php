cdcd3d3e6de160edf76a6e484f8f78955a1e93e8
Fabiano Tomasini <ftomasini@bitbucket.org>
2020-10-03 22:29:07 -0300

    criando migrates e models

--- database/migrations/2020_10_04_005329_create_product_departament.php
+++ database/migrations/2020_10_04_005329_create_product_departament.php
@@ -16,7 +16,10 @@ class CreateProductDepartament extends Migration
         Schema::create('product_departament', function (Blueprint $table) {
             $table->unsignedBigInteger('product_id');
             $table->unsignedBigInteger('departament_id');
-            $table->timestamps();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('departament_id')->references('id')->on('departaments')->onDelete('cascade');
+            $table->primary(['product_id', 'departament_id']);
+            //$table->timestamps();
         });
     }
 
